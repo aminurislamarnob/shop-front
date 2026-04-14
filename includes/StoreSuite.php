349ebd274982f123045601ef26ad6569e03e8c31
Md. Aminur Islam <eng.aminurislam@gmail.com>
2026-04-14 16:09:45 +0600

    :rocket: Release new version v1.0.1

--- includes/StoreSuite.php
+++ includes/StoreSuite.php
@@ -18,7 +18,7 @@ final class StoreSuite {
 	 *
 	 * @var string
 	 */
-	public $version = '1.0.0';
+	public $version = '1.0.1';
 
 	/**
 	 * Instance of self
--- storesuite.php
+++ storesuite.php
@@ -3,7 +3,7 @@
  * Plugin Name: StoreSuite
  * Plugin URI:  https://wordpress.org/plugins/storesuite/
  * Description: Manage your WooCommerce store easily from a front-end dashboard.
- * Version: 1.0.0
+ * Version: 1.0.1
  * Author: Aminur Islam Arnob
  * Author URI: https://github.com/aminurislamarnob/
  * Text Domain: storesuite
